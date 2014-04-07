84cadca254227a986ae5511074164bc423703947
Brooke Bryan <brooke@bajb.net>
2014-04-07 16:14:53 +0100

    Watermark Header

--- src/Views/ComingSoon.php
+++ src/Views/ComingSoon.php
@@ -1,7 +1,19 @@
 <?php
 namespace CubexIo\Views;
 
-class Comingsoon
-{
+use Cubex\View\TemplatedViewModel;
 
+class ComingSoon extends TemplatedViewModel
+{
+  protected $_feature;
+
+  public function __construct($feature)
+  {
+    $this->_feature = $feature;
+  }
+
+  public function getFeature()
+  {
+    return $this->_feature;
+  }
 }
